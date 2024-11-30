2c4d7c3c32fbda1d3b01a133d83549118485a1d5
Michael Moser <michaeldmoser@gmail.com>
2024-11-30 08:26:24 -0700

    Configure vite to use tsconfig paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import vike from "vike/plugin";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [vike({}), react({})],
-  resolve: {
-    alias: {
-      "@": new URL("./", import.meta.url).pathname,
-    },
-  },
+  plugins: [vike({}), react({}), viteTsconfigPaths()],
 });
